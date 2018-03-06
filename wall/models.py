484a39c4982875fb8881046e91cb90f9246716cc
codelioness <kristincodes@gmail.com>
2018-03-06 15:06:31 -0600

    Added color options for posts (default is white)

--- wall/models.py
+++ wall/models.py
@@ -5,6 +5,16 @@ class Post(models.Model):
     post_text = models.TextField(max_length=400)
     author_name = models.CharField(max_length=40)
     pub_date = models.DateTimeField('date published')
+    COLOR_CHOICES = (
+        ('#eaadaa', 'Red'),
+        ('#c9e2ee', 'Blue'),
+        ('#fbf5c5', 'Yellow'),
+        ('#bfe5ac', 'Green'),
+        ('#FFE0B1', 'Orange'),
+        ('#C0B4E7', 'Purple'),
+        ('#FFFFFF', 'White'),
+    )
+    color = models.CharField(max_length=7, choices=COLOR_CHOICES, default='#FFFFFF')
     
     def __str__(self):
         return self.post_text
--- wall/views.py
+++ wall/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
 
@@ -7,16 +7,17 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-    post_list = Post.objects.order_by('-pub_date')[:5]
-    context = {'post_list': post_list,}
+    post_list = Post.objects.order_by('-pub_date')[:20]
+    context = {'post_list': post_list}
     return render(request, 'wall/index.html', context)
 
 def new_post(request):
-    return render(request, 'wall/post.html')
+    context = {'colors': Post.COLOR_CHOICES}
+    return render(request, 'wall/post.html', context)
 
 def posted(request):
     try:
-        post = Post(post_text=request.POST['message'], author_name=request.POST['name'], pub_date=timezone.now())
+        post = Post(post_text=request.POST['message'], author_name=request.POST['name'], pub_date=timezone.now(), color=request.POST['color'])
     except KeyError:
         return render(request, 'wall/post.html', {'error_message': "You forgot something!"})
     else:
